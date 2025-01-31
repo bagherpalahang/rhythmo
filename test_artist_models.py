20a837b48f97a3f12582c5885a6f682cfa204bd0
mohammadaminjohari <107943522+mohammadaminjohari@users.noreply.github.com>
2025-01-31 17:44:18 +0330

    Delete test_artist_models.py

--- test_artist_models.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from core.artists.models import Artist
-
-@pytest.mark.django_db
-def test_create_artist():
-    artist = Artist.objects.create(name="Test Artist", genre="Rock")
-    assert artist.name == "Test Artist"
-    assert artist.genre == "Rock"
