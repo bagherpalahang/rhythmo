388543a47269e862da67577f54164f823efcb87c
mohammadaminjohari <107943522+mohammadaminjohari@users.noreply.github.com>
2025-01-31 17:43:22 +0330

    Delete test_artists_serializers.py

--- test_artists_serializers.py
+++
@@ -1,8 +0,0 @@
-from core.artists.serializers import ArtistSerializer
-from core.artists.models import Artist
-
-def test_artist_serializer():
-    artist = Artist(name="Test Artist", genre="Rock")
-    serializer = ArtistSerializer(instance=artist)
-    assert serializer.data["name"] == "Test Artist"
-    assert serializer.data["genre"] == "Rock"
