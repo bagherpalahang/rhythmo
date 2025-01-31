34ce217c886ec58babbaa0c415bc322f2ff925bc
mohammadaminjohari <107943522+mohammadaminjohari@users.noreply.github.com>
2025-01-31 17:42:48 +0330

    Delete test_search_models.py

--- test_search_models.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from core.search.models import SearchQuery
-
-@pytest.mark.django_db
-def test_create_search_query():
-    query = SearchQuery.objects.create(query_text="test query")
-    assert query.query_text == "test query"
-    assert query.timestamp is not None
